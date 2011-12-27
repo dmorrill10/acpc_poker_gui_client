6603d66d896a331f295d6da3d5ace4436c7e832b
Dustin Morrill <morrill@ualberta.ca>
2011-12-27 03:39:19 -0700

    Fixed style stuff. Improved the organization of the styles a bit. Should be about ready to go online.

--- app/helpers/player_actions_helper.rb
+++ app/helpers/player_actions_helper.rb
@@ -101,8 +101,7 @@ module PlayerActionsHelper
       @round = @match_state.round
 
       # What are the board cards?
-      # @todo Need the game def's number of rounds to do this properly
-      @board_cards = (0..4).inject(BoardCards.new([0, 3, 1, 1])) { |board_cards, i| board_cards << Card.new }
+      @board_cards = BoardCards.new []
       @match_state.board_cards.each_index do |i|
          @board_cards[i] = @match_state.board_cards[i]
       end

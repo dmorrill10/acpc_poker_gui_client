00286d498b7e24406b9f1495e84f94423dbfcd7c
Dustin <morrill@ualberta.ca>
2012-06-25 19:07:03 -0600

    Added a script for setting up MongoDB and fixed some lines in the testing server start script.

---
+++ script/install_mongodb.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+repoDir=$( cd "$( dirname "$0" )" && pwd )
+cd $repoDir
+
+echo -n 'Downloading MongoDb 2.0.6...'
+wget http://fastdl.mongodb.org/linux/mongodb-linux-x86_64-2.0.6.tgz
+echo 'Done'
+
+echo -n 'Unpacking MongoDb...'
+gunzip mongodb-linux-x86_64-2.0.6.tgz
+tar xvf mongodb-linux-x86_64-2.0.6.tar
+rm -f mongodb-linux-x86_64-2.0.6.tar
+mv mongodb-linux-x86_64-2.0.6 mongoDB
+mkdir mongoDB
+mkdir mongoDB/data
+mkdir mongoDB/data/db
+echo 'Done'
+
+cd -
--- script/setup_testing_server.sh
+++ script/setup_testing_server.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_DIR=$( cd "$( dirname "$0" )" && pwd )
 RAILS_ROOT="$SCRIPT_DIR/../"
-$RAILS_ROOT/mongoDB/mongodb-linux-x86_64-2.0.1/bin/mongod --nojournal --dbpath $RAILS_ROOT/mongoDB/data/db &
+$SCRIPT_DIR/mongoDB/bin/mongod --nojournal --dbpath $SCRIPT_DIR/mongoDB/data/db &
 $SCRIPT_DIR/beanstalkd &
 stalk $SCRIPT_DIR/worker.rb &
 cd $RAILS_ROOT

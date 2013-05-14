817e5d4d90dbb1933cd08a82ac5bc716efcefa42
Dustin Morrill <dmorrill10@gmail.com>
2013-05-14 10:52:52 -0600

    Added tests for mongoid extension of ChipStack

---
+++ lib/mongoid_ext/chip_stack.rb
@@ -0,0 +1,18 @@
+require 'acpc_poker_types/chip_stack'
+
+class ChipStack
+  class << self
+    def demongoize(rational)
+      ChipStack.new(rational)
+    end
+    def mongoize(object)
+      case object
+      when ChipStack then object.mongoize
+      else object
+      end
+    end
+    def evolve(object)
+      mongoize(object)
+    end
+  end
+end

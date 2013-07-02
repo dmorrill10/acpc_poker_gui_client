819013e1cd6dc4d87502b65c5b676d1ef57aaaa8
Dustin Morrill <dmorrill10@gmail.com>
2013-07-01 22:18:34 -0600

    Removed unnecessary script.

--- lib/background/worker.rb
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env ruby
-
-require_relative 'table_manager'
-
-# Replace localhost with whatever domain on which you host this app
-AcpcPokerGuiClient::TableManager.listen_to_gui('http://localhost:3000')
